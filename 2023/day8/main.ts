60c84ff0c932071772aea6db03bbe96654f7aca2
simonoob <simonefiorentini1@gmail.com>
2023-12-21 15:59:45 +0200

    chore: add sample data

--- 2023/day8/main.ts
+++ 2023/day8/main.ts
@@ -1,6 +1,14 @@
-import { text } from "./text";
+// import { text } from "./text";
 
-const sample = ``;
+const sample = `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`;
 
 const part1 = (input: string) => {
   const lines = input.split(/\n/);
@@ -9,15 +17,15 @@ const part1 = (input: string) => {
 console.log("part 1:");
 console.log("sample data:", part1(sample));
 console.log("-------------------------------");
-console.log("input data:", part1(text));
-console.log("-------------------------------");
+// console.log("input data:", part1(text));
+// console.log("-------------------------------");
 
 const part2 = (input: string) => {
   const lines = input.split(/\n/);
 };
 
-console.log("part 2:");
-console.log("sample data:", part2(sample));
-console.log("-------------------------------");
-console.log("input data:", part2(text));
-console.log("-------------------------------");
+// console.log("part 2:");
+// console.log("sample data:", part2(sample));
+// console.log("-------------------------------");
+// console.log("input data:", part2(text));
+// console.log("-------------------------------");
