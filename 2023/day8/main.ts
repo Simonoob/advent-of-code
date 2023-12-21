0dfee84eacf0d30f2d3653f547f178556451d6f5
simonoob <simonefiorentini1@gmail.com>
2023-12-21 15:59:03 +0200

    chore: initial setup for day 8

---
+++ 2023/day8/main.ts
@@ -0,0 +1,23 @@
+import { text } from "./text";
+
+const sample = ``;
+
+const part1 = (input: string) => {
+  const lines = input.split(/\n/);
+};
+
+console.log("part 1:");
+console.log("sample data:", part1(sample));
+console.log("-------------------------------");
+console.log("input data:", part1(text));
+console.log("-------------------------------");
+
+const part2 = (input: string) => {
+  const lines = input.split(/\n/);
+};
+
+console.log("part 2:");
+console.log("sample data:", part2(sample));
+console.log("-------------------------------");
+console.log("input data:", part2(text));
+console.log("-------------------------------");
