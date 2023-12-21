bf06d6fb49a6157062a4ec06d0844f0a05e849ab
simonoob <simonefiorentini1@gmail.com>
2023-12-21 17:02:02 +0200

    feat: day 8 part 1

--- 2023/day8/main.ts
+++ 2023/day8/main.ts
@@ -1,24 +1,71 @@
-// import { text } from "./text";
+import { text } from "./text";
 
-const sample = `RL
+const sample = `LLR
 
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
+
+interface INode {
+  id: string;
+  left: string;
+  right: string;
+}
+
+const parseNode = (nodeLine: string): INode => {
+  const parts = nodeLine.split(/=/);
+  const id = parts[0].trim();
+  const leftRight = parts[1]
+    .trim()
+    .replace(/\(|\)|,/g, "")
+    .split(/\s+/);
+  const left = leftRight[0];
+  const right = leftRight[1];
+  return {
+    id,
+    left,
+    right,
+  };
+};
+
+const walk = (instructions: string, nodes: INode[]) => {
+  let currentIndex = 0;
+  let walkedSteps = 0;
+  const startingNode = nodes.find((node) => node.id === "AAA")!;
+  let currentNode = startingNode;
+  // console.log({ instructions });
+
+  while (currentNode.id !== "ZZZ") {
+    const direction = instructions[currentIndex] === "L" ? "left" : "right";
+    // console.log(
+    //   { walkedSteps },
+    //   { currentIndex },
+    //   { direction },
+    //   { currentNode },
+    // );
+    currentNode = nodes.find((node) => node.id === currentNode[direction])!;
+
+    walkedSteps += 1;
+    currentIndex = (currentIndex + 1) % instructions.length;
+  }
+  return walkedSteps;
+};
 
 const part1 = (input: string) => {
   const lines = input.split(/\n/);
+  const instructions = lines[0];
+  lines.splice(0, 2);
+
+  const nodes = lines.map((line) => parseNode(line));
+
+  return walk(instructions, nodes);
 };
 
 console.log("part 1:");
 console.log("sample data:", part1(sample));
 console.log("-------------------------------");
-// console.log("input data:", part1(text));
-// console.log("-------------------------------");
+console.log("input data:", part1(text));
+console.log("-------------------------------");
 
 const part2 = (input: string) => {
   const lines = input.split(/\n/);
